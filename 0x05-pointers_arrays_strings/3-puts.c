ed924fd5540d6ed04bf4ec4398601200c5fb6456
Mostafa_Shebl <mostafa.shebl18@gmail.com>
2023-02-28 07:26:04 -0800

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,11 @@
  * @str: pointer the string to print
  * Return: void
  */
-void _puts(char *str);
+void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i])
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str++);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
